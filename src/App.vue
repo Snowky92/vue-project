9130bb693777c8eeb0e85ecefa42e968a0918e67
Snowky92 <amaurypinheiro@hotmail.fr>
2022-09-23 14:29:47 +0200

    tableau fonctionnel

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,70 @@
-<script setup>
+<script>
+  export default {
+    data() {
+      return {
+        date : null,
+        montant : null,
+        description : null,
+        categorie : ("(à venir)"),
+        list : [
 
+        ]
+      }
+    },
+    methods : {
+      save(){
+        this.list.push({
+          date : this.date,
+          montant : this.montant,
+          description : this.description,
+          categorie : this.categorie,
+        })
+
+        this.date = null;
+        this.montant = null;
+        this.description = null;
+      }
+    }
+  }
 </script>
 
 
 <template>
 
-<div class="table">
+<div>
   <table border="1">
-    <tr>
-      <th>Date</th><th>Montant</th><th>Description</th><th>Catégorie</th>
+    <thead>
+      <tr>
+        <th>Date</th>
+        <th>Montant</th>
+        <th>Description</th>
+        <th>Catégorie</th>
+      </tr>
+    </thead>
+    <tbody>
+    <tr v-for="reponse in list">
+      <td>{{ reponse.date }}</td>
+      <td>{{ reponse.montant}} €</td>
+      <td>{{ reponse.description}}</td>
+      <td>{{ reponse.categorie}}</td>
     </tr>
-    <tr>
-    </tr>
+    </tbody>
   </table>
 
-  <form action="" method="get" class="form">
-    <label>Date : </label>
-    <input v-model="date" type="date" name="date" id="date" required><br>
-    <label>Montant : </label>
-    <input type="text" name="montant" id="montant" required><br>
-    <label>Description : </label>
-    <input type="text" name="description" id="description" required><br>
-    <input type="submit" value="save">
-  </form>
+ 
+    Date : <input v-model="date" type="date"><br>
+    Montant : <input v-model="montant"><br>
+    Description : <input v-model="description"><br>
+    <button class="bouton" v-on:click="save">save</button>
+
 </div>
 
 </template>
 
 <style>
+
+.bouton{
+  background-color: aqua;
+}
 
 </style>
