89554a8722538587c27c320a491ab1c50dd81a92
Snowky92 <amaurypinheiro@hotmail.fr>
2022-09-23 11:59:20 +0200

    création du tableau et du form et html

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,31 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 
+<template>
 
+<div class="table">
+  <table>
+    <tr>
+      <th>Date</th>
+      <th>Montant</th>
+      <th>Description</th>
+      <th>Catégorie</th>
+    </tr>
+  </table>
+
+  <form action="" method="get" class="form">
+    <label>Date : </label>
+    <input type="date" name="date" id="date" required><br>
+    <label>Montant : </label>
+    <input type="text" name="montant" id="montant" required><br>
+    <label>Description : </label>
+    <input type="text" name="description" id="description" required><br>
+    <input type="submit" value="Enregistrer">
+  </form>
+</div>
+
+</template>
+
+<style>
+</style>
